a22277db428fdb42bd8a63036e6780da32f1cc9f
linjun <425227665@qq.com>
2022-12-12 14:16:07 +0800

    test

---
+++ src/main/java/com/example/hellowworld/Hellow.java
@@ -0,0 +1,11 @@
+package com.example.hellowworld;
+
+public class Hellow {
+
+    public String test() {
+        String a = "你好";
+        System.out.println(a);
+        return a;
+    }
+
+}
---
+++ src/test/java/com/example/hellowworld/HellowWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellowworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellowWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
